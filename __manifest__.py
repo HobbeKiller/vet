7512c493ca27e6d928a86d5b3f961553bf032bb6
HobbeKiller <alutaev@gmail.com>
2026-04-14 10:19:44 +0200

    Reorder security files in __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -19,7 +19,9 @@ Features:
     'version': '18.0.1.0.0',
     'depends': ['base', 'mail', 'contacts', 'account', 'sale_management'],
     'data': [
-        'security/ir.model.access.csv',
+        'security/groups.xml',          # 👈 PRIMERO
+        'security/ir.model.access.csv', # 👈 DESPUÉS
+        'security/rules.xml',
         'views/animals_views.xml',
         'views/medicines_views.xml',
         'views/allergies_views.xml',
